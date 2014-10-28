fad0274b0c2d72460475bcb64af480a49e77b940
dtaniwaki <daisuketaniwaki@gmail.com>
2014-10-29 00:58:23 +0900

    Public around_validation

--- lib/state_machine/integrations/active_model.rb
+++ lib/state_machine/integrations/active_model.rb
@@ -513,6 +513,7 @@ module StateMachine
         def around_validation(object)
           object.class.state_machines.transitions(object, action, :after => false).perform { yield }
         end
+        public :around_validation
         
         # Creates a new callback in the callback chain, always inserting it
         # before the default Observer callbacks that were created after
